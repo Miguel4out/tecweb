ed0c24b8cb49751ee58cdfb4edf3217a59d75d4a
Miguel4out <miguel_a.m99@hotmail.es>
2025-09-24 11:54:28 -0600

    Creación de la carpeta p07

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -29,6 +29,88 @@
         echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
         echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
         echo '</ul>';
+
+        $a = "ManejadorSQL";
+        $b = "MySQL";
+        $c = &$a;
+
+        echo "<li> $c";
+
+        $a = "PHP server";
+        $b = &$a;
+    
+        echo "<li> $b </li>" ;
+        $a = "PHP5";
+        echo "<li> $a </li>";
+        $z[] = &$a;
+        $b = "5a version de PHP";
+        echo "<li> $b </li>";
+        $c = $b*10;
+        echo "<li> $c </li>";
+        $a .= $b;
+        echo "<li> $a </li>";
+        $b *= $c;
+        echo "<li> $b </li>";
+        $z[0] = "MySQL";
+        foreach($z as $item){
+            echo "<li>$item</li>";
+        };
+
+        function mostrarVariables() {
+        global $a, $b, $c, $z;
+
+        echo "<h3>Valores usando global</h3>";
+        echo "<ul>";
+        echo "<li>\$a = $a</li>";
+        echo "<li>\$b = $b</li>";
+        echo "<li>\$c = $c</li>";
+
+            foreach ($z as $key => $value) {
+                echo "<li>\$z[$key] = $value</li>";
+            }
+
+        echo "</ul>";
+        }
+
+        mostrarVariables();
+
+    $a = "7 personas";
+    echo "<li>$a </li>";
+    $b = (integer) $a;
+    echo "<li>$b </li>";
+    $a = "9E3";
+    echo "<li>$a </li>";
+    $c = (double) $a;
+    echo "<li>$c </li>";
+
+
+    $a = "0";
+    $b = "TRUE";
+    $c = FALSE;
+    $d = ($a OR $b);
+    $e = ($a AND $c);
+    $f = ($a XOR $b);
+
+    echo "<pre>";
+        var_dump($a);
+        var_dump($b);
+        var_dump($c);
+        var_dump($d);
+        var_dump($e);
+        var_dump($f);
+    echo "</pre>";
+
+    echo "<li>\$c = " . var_export($c, true) . "</li>";
+    echo "<li>\$e = " . var_export($e, true) . "</li>";
+
+
+    echo '<br>Versión de PHP: ' . phpversion();
+    echo '<br>Software del servidor: ' . $_SERVER['SERVER_SOFTWARE'];
+    echo '<br>Sistema operativo del servidor: ' . PHP_OS;
+    echo '<br>Idioma del navegador: ' . $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+
+
+
     ?>
 </body>
 </html>
